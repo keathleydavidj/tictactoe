6bf37e387fae9c45ae95a2b4db1db72df437c60d
David Keathley <keathleydavidj@gmail.com>
2015-12-16 15:25:05 -0600

    working MVP

--- js/main.js
+++ js/main.js
@@ -1,64 +1,139 @@
 $(document).ready(function() {
-  var turn = 0;
-  var p1 = 'x';
-  var p2 = 'o';
-  $('.gm-piece').on('click', function() {
-    turn++
-    console.log(turn);
-  });
 
-  $('#btn0').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn0').html("o");
-    else 
-      $('#btn0').html("x");
-  });
-  $('#btn1').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn1').html("o");
-    else 
-      $('#btn1').html("x");
-  });
-  $('#btn2').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn2').html("o");
-    else 
-      $('#btn2').html("x");
-  });
-  $('#btn3').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn3').html("o");
-    else 
-      $('#btn3').html("x");
-  });
-  $('#btn4').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn4').html("o");
-    else 
-      $('#btn4').html("x");
-  });
-  $('#btn5').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn5').html("o");
-    else 
-      $('#btn5').html("x");
-  });
-  $('#btn6').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn6').html("o");
-    else 
-      $('#btn6').html("x");
-  });
-  $('#btn7').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn7').html("o");
-    else 
-      $('#btn7').html("x");
-  });
-  $('#btn8').on('click', function() {
-    if (turn%2 == 0)
-      $('#btn8').html("o");
-    else 
-      $('#btn8').html("x");
-  });
+    var sq1 = $('#btn1');
+    var sq2 = $('#btn2');
+    var sq3 = $('#btn3');
+    var sq4 = $('#btn4');
+    var sq5 = $('#btn5');
+    var sq6 = $('#btn6');
+    var sq7 = $('#btn7');
+    var sq8 = $('#btn8');
+    var sq9 = $('#btn9');
+
+    var playValid = false;
+    var win = false;
+    var turn = 0;
+
+
+    $('.gm-piece').on('click', function() {
+        turn++;
+        if (turn % 2 != 0) {
+
+
+            validatePlay(this);
+
+            if (playValid) {
+                $(this).removeClass('free');
+                $(this).addClass('played');
+                $(this).addClass('X-play');
+                $(this).html("X");
+
+                checkDraw();
+                checkWin();
+
+            } else {
+                alert("That square has already been played. Please choose another square");
+            }
+
+        } else {
+
+
+            validatePlay(this);
+
+            if (playValid) {
+                $(this).removeClass('free');
+                $(this).addClass('played');
+                $(this).addClass('O-play');
+                $(this).html("O");
+
+                checkDraw();
+                checkWin();
+
+            } else {
+                alert("That square has already been played. Please choose another square");
+            }
+
+        }
+    });
+
+    function validatePlay(squareplayed) {
+        playValid = true;
+        // if ($(squareplayed).hasClass('free')) {
+        //     playValid = true;
+        // } else {
+        //     playValid = false;
+        //     return false;
+        // }
+    }
+
+    function clearBoard() {
+        $('.gm-piece').removeClass('played');
+        $('.gm-piece').removeClass('O-play');
+        $('.gm-piece').removeClass('X-play');
+        $('.gm-piece').html('');
+        $('.gm-piece').addClass('free');
+        turn = 0;
+    }
+
+    function winAlert(player) {
+        win = true;
+
+        if (player == "X") {
+            alert("Player X wins!")
+        } else {
+            alert("Player O wins!")
+        }
+        clearBoard();
+    }
+
+    function checkWin() {
+
+        if (sq1.hasClass('X-play') && sq2.hasClass('X-play') && sq3.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq1.hasClass('O-play') && sq2.hasClass('O-play') && sq3.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq4.hasClass('X-play') && sq5.hasClass('X-play') && sq6.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq4.hasClass('O-play') && sq5.hasClass('O-play') && sq6.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq7.hasClass('X-play') && sq8.hasClass('X-play') && sq9.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq7.hasClass('O-play') && sq8.hasClass('O-play') && sq9.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq1.hasClass('X-play') && sq4.hasClass('X-play') && sq7.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq1.hasClass('O-play') && sq4.hasClass('O-play') && sq7.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq5.hasClass('X-play') && sq2.hasClass('X-play') && sq8.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq5.hasClass('O-play') && sq2.hasClass('O-play') && sq8.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq6.hasClass('X-play') && sq9.hasClass('X-play') && sq3.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq6.hasClass('O-play') && sq9.hasClass('O-play') && sq3.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq1.hasClass('X-play') && sq5.hasClass('X-play') && sq9.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq1.hasClass('O-play') && sq5.hasClass('O-play') && sq9.hasClass('O-play')) {
+            winAlert("O");
+        } else if (sq5.hasClass('X-play') && sq7.hasClass('X-play') && sq3.hasClass('X-play')) {
+            winAlert("X");
+        } else if (sq5.hasClass('O-play') && sq7.hasClass('O-play') && sq3.hasClass('O-play')) {
+            winAlert("O");
+        }
+    }
+
+    function checkDraw() {
+
+        if (!($('.gm-piece').hasClass('free'))) {
+            alert("Draw! Try playing again!");
+            clearBoard();
+        }
+    }
+
+    $('#reset-btn').on('click', function() {
+        clearBoard();
+    })
+
+
 });
